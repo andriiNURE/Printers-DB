66ef9561eb7e4ca846d58dd31c413995dfea0eec
andriiNURE <70981932+andriiNURE@users.noreply.github.com>
2020-12-29 15:25:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Printers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy proxy = new Proxy("admin", "admin");
+
+            DAOFactory dAOFactory = new DAOFactory();
+
+            Printer printer = new Printer.Builder("anotherOne", 3).computerId(3).Built();
+
+            dAOFactory.GetPrinterDAO().Create(printer, proxy);
+        }
+    }
+}
